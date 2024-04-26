cf000d0b062ef1e3dcfbedc9cc0f3bef649a34cb
scorsi <8389441+scorsi@users.noreply.github.com>
2024-04-26 17:24:34 +0200

    feat: change lint keybinds

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -17,7 +17,7 @@ return {
             end,
         })
 
-        vim.keymap.set("n", "<leader>l", function()
+        vim.keymap.set("n", "<leader>ml", function()
             lint.try_lint()
         end, { desc = "Trigger linting for current file" })
     end,
