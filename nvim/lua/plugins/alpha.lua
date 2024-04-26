03f1461ab1f468c9f85da9883316d438ead5da64
scorsi <8389441+scorsi@users.noreply.github.com>
2024-04-26 15:30:16 +0200

    feat: update alpha dashboard

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,32 +1,42 @@
 return {
-    {
-        "goolord/alpha-nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-            local alpha = require("alpha")
-            local dashboard = require("alpha.themes.startify")
+	{
+		"goolord/alpha-nvim",
+		event = "VimEnter",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			local alpha = require("alpha")
+			local dashboard = require("alpha.themes.dashboard")
 
-            dashboard.section.header.val = {
-                [[                                                                       ]],
-                [[                                                                       ]],
-                [[                                                                       ]],
-                [[                                                                       ]],
-                [[                                                                     ]],
-                [[       ████ ██████           █████      ██                     ]],
-                [[      ███████████             █████                             ]],
-                [[      █████████ ███████████████████ ███   ███████████   ]],
-                [[     █████████  ███    █████████████ █████ ██████████████   ]],
-                [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-                [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-                [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-                [[                                                                       ]],
-                [[                                                                       ]],
-                [[                                                                       ]],
-            }
+			dashboard.section.header.val = {
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                     ]],
+				[[       ████ ██████           █████      ██                     ]],
+				[[      ███████████             █████                             ]],
+				[[      █████████ ███████████████████ ███   ███████████   ]],
+				[[     █████████  ███    █████████████ █████ ██████████████   ]],
+				[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+				[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+				[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+			}
 
-            alpha.setup(dashboard.opts)
-        end,
-    },
+			dashboard.section.buttons.val = {
+				dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+				dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree filesystem reveal left toggle<CR>"),
+				dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+				dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+				dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+				dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+			}
+
+			alpha.setup(dashboard.opts)
+		end,
+	},
 }
