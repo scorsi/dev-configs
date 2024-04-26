37ee3079092fd44b99bddf102420518d36634e16
scorsi <8389441+scorsi@users.noreply.github.com>
2024-04-26 17:37:56 +0200

    fix: missing space in alpha dashboard

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,42 +1,46 @@
 return {
-	{
-		"goolord/alpha-nvim",
-		event = "VimEnter",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			local alpha = require("alpha")
-			local dashboard = require("alpha.themes.dashboard")
+    {
+        "goolord/alpha-nvim",
+        event = "VimEnter",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.dashboard")
 
-			dashboard.section.header.val = {
-				[[                                                                       ]],
-				[[                                                                       ]],
-				[[                                                                       ]],
-				[[                                                                       ]],
-				[[                                                                     ]],
-				[[       ████ ██████           █████      ██                     ]],
-				[[      ███████████             █████                             ]],
-				[[      █████████ ███████████████████ ███   ███████████   ]],
-				[[     █████████  ███    █████████████ █████ ██████████████   ]],
-				[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-				[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-				[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-				[[                                                                       ]],
-				[[                                                                       ]],
-				[[                                                                       ]],
-			}
+            dashboard.section.header.val = {
+                [[                                                                       ]],
+                [[                                                                       ]],
+                [[                                                                       ]],
+                [[                                                                       ]],
+                [[                                                                     ]],
+                [[       ████ ██████           █████      ██                     ]],
+                [[      ███████████             █████                             ]],
+                [[      █████████ ███████████████████ ███   ███████████   ]],
+                [[     █████████  ███    █████████████ █████ ██████████████   ]],
+                [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+                [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+                [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+                [[                                                                       ]],
+                [[                                                                       ]],
+                [[                                                                       ]],
+            }
 
-			dashboard.section.buttons.val = {
-				dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-				dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree filesystem reveal left toggle<CR>"),
-				dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-				dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-				dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-				dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
-			}
+            dashboard.section.buttons.val = {
+                dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+                dashboard.button(
+                    "SPC ee",
+                    "  > Toggle file explorer",
+                    "<cmd>Neotree filesystem reveal left toggle<CR>"
+                ),
+                dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+                dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+                dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+                dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+            }
 
-			alpha.setup(dashboard.opts)
-		end,
-	},
+            alpha.setup(dashboard.opts)
+        end,
+    },
 }
