b536cff86790786d1b0e956260b911ba76675c79
scorsi <8389441+scorsi@users.noreply.github.com>
2024-04-27 01:57:37 +0200

    feat: add fzf fish plugin

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set fzf_preview_dir_cmd eza --tree --color=always
+# set fzf_preview_file_cmd bat
+set fzf_diff_highlighter delta --paging=never --width=20
+
 fzf --fish | source
 zoxide init fish | source
 
