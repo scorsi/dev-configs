2bf17ac54b710599c934097359a5a226fa85bbb2
scorsi <8389441+scorsi@users.noreply.github.com>
2024-04-26 15:27:49 +0200

    feat: change neotree keybinds

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,41 @@
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+			{
+				"s1n7ax/nvim-window-picker",
+				version = "2.*",
+				config = function()
+					require("window-picker").setup({
+						filter_rules = {
+							include_current_win = false,
+							autoselect_one = true,
+							bo = {
+								filetype = { "neo-tree", "neo-tree-popup", "notify" },
+								buftype = { "terminal", "quickfix" },
+							},
+						},
+					})
+				end,
+			},
+		},
+		config = function()
+			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
+
+            vim.keymap.set("n", "<leader>ee", ":Neotree filesystem reveal left toggle<CR>", {})
+			vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+
+			require("neo-tree").setup({})
+
+			vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+		end,
+	},
+}
