c368a48099d4db53dde8200d2cf714d2510d6022
scorsi <8389441+scorsi@users.noreply.github.com>
2024-04-27 16:14:23 +0200

    feat: remove treesitter vim highlighting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -11,8 +11,13 @@ return {
                     "rust",
                     "python",
                 },
-                highlight = { enable = true },
-                indent = { enable = true },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = {
+                    enable = true,
+                },
             })
         end,
     },
