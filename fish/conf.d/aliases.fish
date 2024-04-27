545e08d0a1913642f46390e8feaafe32f2e8d5cc
scorsi <8389441+scorsi@users.noreply.github.com>
2024-04-27 02:43:16 +0200

    feat: change aliases to leverage exa, zoxide and bat

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,11 +1,11 @@
 alias ls "exa --color=always --icons=always -A"
 alias ll "ls --long --no-filesize --no-time --no-user --no-permissions --git"
 alias lt "ll --tree"
-alias g git
+alias g "git"
 
-alias cat bat
-alias cd z
+alias cat "bat"
+alias zz "z -"
 
-alias vim nvim
-alias vi vim
+alias vim "nvim"
+alias vi "vim"
 
