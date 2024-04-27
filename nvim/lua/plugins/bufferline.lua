84bc16e98fc007ac211c2cd533db9d87bff8ce7f
scorsi <8389441+scorsi@users.noreply.github.com>
2024-04-27 18:20:23 +0200

    feat: better bufferline options

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -4,8 +4,16 @@ return {
     version = "*",
     opts = {
         options = {
-            mode = "tabs",
-            separator_style = "slant",
+            numbers = "buffer id",
+            diagnostics = "nvim_lsp",
+            diagnostics_indicator = function(count, level, diagnostics_dict, context)
+                local s = " "
+                for e, n in pairs(diagnostics_dict) do
+                    local sym = e == "error" and " " or (e == "warning" and " " or "")
+                    s = s .. n .. sym
+                end
+                return s
+            end,
         },
     },
 }
