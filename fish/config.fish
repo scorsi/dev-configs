0fa5b0b1645ac69ba6dde080ac0a9a173a74976e
scorsi <8389441+scorsi@users.noreply.github.com>
2024-04-29 15:31:02 +0200

    feat: configure sponge to clear only on session exit

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set sponge_purge_only_on_exit true
 set fzf_preview_dir_cmd eza --tree --color=always
 # set fzf_preview_file_cmd bat
 set fzf_diff_highlighter delta --paging=never --width=20
