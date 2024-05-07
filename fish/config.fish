326021938f4c4d694f91b2e9c6b38a601c4b42d2
scorsi <8389441+scorsi@users.noreply.github.com>
2024-05-07 11:54:20 +0200

    feat: add support for pnpm

--- fish/config.fish
+++ fish/config.fish
@@ -16,3 +16,10 @@ zoxide init fish | source
 
 starship init fish | source
 
+
+# pnpm
+set -gx PNPM_HOME "/home/scorsi/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
